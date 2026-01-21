ed65d119bb72665694169bd56209b8d93ac0bc31
Samruddhi Vinod Yerekar <samruddhiyerekar3@gmail.com>
2026-01-21 21:04:28 +0530

    Add even-odd check in days02_even_odd.cpp

---
+++ days02_even_odd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNum = 5;
+  
+  if(myNum % 2==0){
+    cout<< myNum <<"is even.\n";
+  }else{
+    cout<< myNum <<"is odd.\n";
+  }
+return 0;
+}
