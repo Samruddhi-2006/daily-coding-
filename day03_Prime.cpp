b338330fde4a0c440d90190d9d8314a6439caf92
Samruddhi Vinod Yerekar <samruddhiyerekar3@gmail.com>
2026-01-22 20:28:44 +0530

    Implement prime number check in day03_Prime.cpp

---
+++ day03_Prime.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int n, i;
+  cout<<"Enter number:";
+  cin>>n;
+
+  for(i = 2; i < n; i++) {
+    if(n % i == 0) {
+      cout<<"Not prime";
+      return 0;
+    }
+  }
+  cout<<"Prime";
+  return 0;
+}
