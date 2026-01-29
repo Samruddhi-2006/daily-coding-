8fcd73b1d246cdebb98860e39d1bab104ea552c8
Samruddhi Vinod Yerekar <samruddhiyerekar3@gmail.com>
2026-01-29 15:07:09 +0530

    Implement basic calculator functionality in C++

---
+++ day09_calculator.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+  int a, b;
+  char op;
+  cout<<"Enter first number:";
+  cin>>a;
+
+  cout<<"Enter second number:";
+  cin>>b;
+
+  cout<<"Enter operator(+ - * /):";
+  cin>>op;
+
+  switch(op){
+    case'+':
+        cout<<"Result="<<a+b;
+        break;
+    case'-':
+        cout<<"Result="a-b;
+        break;
+    case'*':
+        cout<<"Result="a*b;
+        break;
+    case'/':
+        if(b!=0)
+            cout<<"Result="<<a/b;
+        else
+            cout<<"Division by zero not allowed";
+        break;
+    default:
+      cout<<"Invalid operator";
+  }
+  return 0;
+}
